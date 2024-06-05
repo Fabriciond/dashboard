3aae438528e74393c7b9f3e0faa7ea2cebf4c50d
Fabricio Nuñez <78011988+Fabriciond@users.noreply.github.com>
2024-06-05 15:40:25 -0700

    agregue estilos a los filtros
    se agregaron estilos principales para ir dando forma al proyecto

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,22 @@
-
-
-
-
-
+import './components.css';
 export const FiltersComponent = () => {
     return(
         <>
-            <h1>Filters</h1>
+           <div className="wrapper-filters">
+                <div className="filters">
+                    <div className="filter">
+                        <label htmlFor="">Variable Climatica</label>
+                        <select name="" id=""></select>
+                    </div>
+                    <div className="filter">
+                        <label htmlFor="">Fecha</label>
+                        <input type="date" name="" id="" />
+                    </div>
+                    <div className="filter">
+
+                    </div>
+                </div>
+           </div>
         </>
     )
 
